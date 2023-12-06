ec442f40f346c44309c4ab89cba6298b1fc9f082
t-coffe <tcoffe@edenschool.fr>
2023-12-06 15:15:55 +0100

    colors can be clicked

--- main.js
+++ main.js
@@ -4,17 +4,58 @@ const bleu = document.querySelector('.bleu');
 const jaune = document.querySelector('.jaune');
 
 
-vert.style.backgroundColor = "green";
-bleu.style.backgroundColor = "blue";
-jaune.style.backgroundColor = "yellow";
-// rouge.style.backgroundColor = "lightcoral";
+rouge.style.backgroundColor = "lightcoral";
+vert.style.backgroundColor =  "lightgreen";
+bleu.style.backgroundColor =  "lightblue";
+jaune.style.backgroundColor = "lightyellow";
 
 
-rouge.addEventListener('click', ()=> {
-  console.log(rouge.currentStyle);
+const funcRed = ()=> {
   if (rouge.style.backgroundColor === "lightcoral") {
     rouge.style.backgroundColor = "red";
+    rouge.style.border = "solid" ;
   }else {
     rouge.style.backgroundColor = "lightcoral";
+    rouge.style.border = "dashed" ;
   }
-})
+} 
+
+rouge.addEventListener('click', funcRed);
+
+const funcGreen = ()=> {
+  if (vert.style.backgroundColor === "lightgreen") {
+    vert.style.backgroundColor = "green";
+    vert.style.border = "solid" ;
+  }else {
+    vert.style.backgroundColor = "lightgreen";
+    vert.style.border = "dashed" ;
+  }
+} 
+
+vert.addEventListener('click', funcGreen);
+
+
+const funcBlue = ()=> {
+  if (bleu.style.backgroundColor === "lightblue") {
+    bleu.style.backgroundColor = "blue";
+    bleu.style.border = "solid" ;
+  }else {
+    bleu.style.backgroundColor = "lightblue";
+    bleu.style.border = "dashed" ;
+  }
+} 
+
+bleu.addEventListener('click', funcBlue);
+
+
+const funcYellow = ()=> {
+  if (jaune.style.backgroundColor === "lightyellow") {
+    jaune.style.backgroundColor = "yellow";
+    jaune.style.border = "solid" ;
+  }else {
+    jaune.style.backgroundColor = "lightyellow";
+    jaune.style.border = "dashed" ;
+  }
+}
+
+jaune.addEventListener('click', funcYellow);
